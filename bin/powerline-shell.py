23391017237728e960fd30686e2c233000bf0f01
Jonan Scheffler <jonan@heroku.com>
2016-06-02 14:19:01 -0700

    Getting Python setup.

--- bin/powerline-shell.py
+++ bin/powerline-shell.py
@@ -227,6 +227,21 @@ class Color(DefaultColor):
     VIRTUAL_ENV_FG = 00
 
 
+import os
+
+def add_virtual_env_segment():
+    env = os.getenv('VIRTUAL_ENV')
+    if env is None:
+        return
+
+    env_name = os.path.basename(env)
+    bg = Color.VIRTUAL_ENV_BG
+    fg = Color.VIRTUAL_ENV_FG
+    powerline.append(' %s ' % env_name, fg, bg)
+
+add_virtual_env_segment()
+
+
 import subprocess
 
 
